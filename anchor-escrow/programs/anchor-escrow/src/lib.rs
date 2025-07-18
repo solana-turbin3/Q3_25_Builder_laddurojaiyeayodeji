f69ed37b3dbc5f4f6e07132891ddf122e83daed2
mrlad01 <ladayodeji@gmail.com>
2025-07-18 18:20:15 +0100

    update on the escrow: update on lib.rs file

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -18,9 +18,12 @@ pub mod anchor_escrow {
         ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
         ctx.accounts.deposit(deposit)
     }
-    pub fn take(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
-        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
-        ctx.accounts.deposit(deposit)
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close()
+    }
+    pub fn refund_and_close(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close()
     }
 }
 
